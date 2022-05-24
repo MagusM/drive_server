cae1871ba7486334528697c4940b2df48a61c435
simonm <simon@carwiz.co.il>
2022-05-24 22:25:46 +0300

    type orm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: process.env.NODE_ENV == 'dev' ? '.dev.env' : '.env',
-    })
+    }),
+    DatabaseModule
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
-
-// 4.57.55
